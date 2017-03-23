0615c37d51a15905eac6cabd0fdc8f26168c6dfb
sirrow <sirrow@gmail.com>
2017-03-24 00:56:35 +0900

    プロジェクト ファイルを追加します。

---
+++ keyhook_google_translation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace keyhook_google_translation
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
